6b55e9738919473c4c57abb018abe259ca062283
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-02 13:19:14 +0100

    Added Components and Updates README with Resources (#4)
    * Updated the README file to display correct title
    
    * added NPM
    
    * I have created 3 components as a skeleton and have updated the read me file to include links
    
    * moves files with the src folder
    
    * Update on readme for resources

---
+++ src/Help.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Help.css';
+
+class Help extends Component {
+    render() {
+        return (
+            <div className="body">
+                <p className="intro">  This is the Help conponent</p>
+            </div>
+        );
+    }
+}
+
+export default Help;
