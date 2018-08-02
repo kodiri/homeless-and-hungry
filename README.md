a052ba7d1c306184614c8d7e1735ea5abdbfd03a
David Smith <creepingcats@btconnect.com>
2018-08-02 16:07:22 +0100

    Created basic components and css for Clothing, Washing and Main interface

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # The Homeless and Hungry app is designed to help people find food, accommodation, clothing, washing facilities, medical attention and additional places for help and support
 
 #RESOURCES
+
+Icons used are under 
---
+++ src/Interface.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Interface.css';
+import AccommodationIconBlue from './Icons/AccommodationIconBlue.png';
+import FoodIconBlue from './Icons/FoodIconBlue2.png';
+import HelpIconBlue from './Icons/AccommodationIconBlue.png';
+import ClothingIconBlue from './Icons/ClothingIconBlue.png';
+import MedicalIconBlue from './Icons/MedicalIconBlue.png';
+import WashingIconBlue from './Icons/WashingIconBlue.png';
+
+
+
+
+
+class Help extends Component {
+    render() {
+        return (
+            <div className="body">
+                <p className="intro">  This is the main interface to navigate to other parts</p>
+            </div>
+        );
+    }
+}
+
+export default Interface;
