69147510718e6f9d3cc201327c3792df961d1900
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-03 17:21:38 +0100

    updated accondation.js

--- src/Accomodation.js
+++ src/Accomodation.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import './Accomodation.css';
-<<<<<<< HEAD
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import AccommodationIconBlue from './Icons/AccommodationIconBlue.png';
-
-export default class Accommodation extends React.Component {
-    render() {
-        return(
-            <div>
-                <AccommodationIconBlue />
-            </div>
-
-        )
-    }
-=======
-import AccommodationIconBlue from './Icons/AccommodationIconBlue.png';
->>>>>>> upstream/master
 
 export class Accommodation extends React.Component {
     render() {
