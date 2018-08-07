a1f60e3dea68957bf25472eb14cfed953e40ef0a
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-07 10:54:17 +0100

    added the final icons to each component for clothing and washing

--- src/Clothing.js
+++ src/Clothing.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Clothing.css';
+import ClothingIconBlue from './Icons/ClothingIconBlue';
 
 class Help extends Component {
     render() {
         return (
             <div className="body">
+            <ClothingIconBlue />
                 <p className="intro">  This is the Clothing component</p>
             </div>
         );
--- src/Washing.js
+++ src/Washing.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Washing.css';
+import WashingIconBlue from './Icons/WashingIconBlue';
 
 class Help extends Component {
     render() {
         return (
             <div className="body">
+            <WashingIconBlue />
                 <p className="intro">  This is the Washing component</p>
             </div>
         );
