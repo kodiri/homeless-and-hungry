d8c5dc846afed103711c3dd066014f1b65fef1a4
DavidScreative <36810863+DavidScreative@users.noreply.github.com>
2018-08-07 10:27:40 +0100

    Added rough components and css for clothing, washing and main interface (#9)
    * Update of ReadMe to include a few more details
    
    * Amended welcome title to include responsive
    
    * Added PNG icons
    
    * Resources details
    
    * Created basic components and css for Clothing, Washing and Main interface
    
    * Added more info the the README and additional CSS to the main css file

---
+++ src/Washing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Washing.css';
+
+class Help extends Component {
+    render() {
+        return (
+            <div className="body">
+                <p className="intro">  This is the Washing component</p>
+            </div>
+        );
+    }
+}
+
+export default Washing;
