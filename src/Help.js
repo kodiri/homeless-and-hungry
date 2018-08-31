43e3cd6d431d206675b5359796aec98ba8e65e1f
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-31 12:06:14 +0100

    added buttons to each component as well as descriptions (#36)
    * Updated the README file to display correct title
    
    * added NPM
    
    * I have created 3 components as a skeleton and have updated the read me file to include links
    
    * moves files with the src folder
    
    * Update on readme for resources
    
    * added the icons to each component 4 out of 6
    
    * updated accondation.js
    
    * added the final icons to each component for clothing and washing
    
    * added a common folder for the header and about page to help with file structure
    
    * Deleted old png icons
    
    * logo.svg removal
    
    * added addresses for accomidation
    
    * updated each component with svg logo and also created all routing and linking to different pages
    
    * updated component medical with 5 items of information addresses
    
    * added information on the clothing component
    
    * added imformation to the washing component
    
    * added information for accomodation and food
    
    * added information for help component
    
    * changed links for new page
    
    * added links that add to new page when clicked for all components
    
    * added all buttons and description to each component

--- src/Help.js
+++ src/Help.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Help.css';
 import HelpIcon from './Icons/HelpIcon.svg';
+import { Link } from 'react-router-dom';
 
 
 class Help extends Component {
@@ -8,85 +9,95 @@ class Help extends Component {
         return (
             <div className="body">
                 <img src={HelpIcon} className="Icon" alt="Help Icon" />
-                <div className='HelpItems'>
-                    <div className='item'>
-                        <h5>Hope Worldwide</h5>
-                        <p>Advice for homeless people, including referrals to hostels
-                            and assistance with finding private rented accommodation.
+                <div><h1 className="MainHeading">Help</h1>
+                    <div><h1 className="IntroText">Here are your search results for Help.</h1>
+                        <div><h1 className="IntroText">To return to the main menu click on the button at the bottom of the page.</h1>
+                            <div className='HelpItems'>
+                                <div className='item'>
+                                    <h5>Hope Worldwide</h5>
+                                    <p>Advice for homeless people, including referrals to hostels
+                                        and assistance with finding private rented accommodation.
                             Also provide a residential drug and alcohol rehabilitation programme.</p>
-                        <ol>
-                            <li>Opening hours:</li>
-                            <li>Monday – Friday: 10am–4pm</li>
-                            <li>Address: 360-364 City Road, London, EC1V 2PY</li>
-                            <li>Phone: 020 7713 7655</li>
-                            <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.hopeworldwide.org.uk'>http://www.hopeworldwide.org.uk</a></li>
-                        </ol>
-                    </div>
-                </div>
-                <div className='HelpItems'>
-                    <div className='item'>
-                        <h5>All Saints Church, Islington</h5>
-                        <p>Food and social contact for people who are homeless and on the margins.
-                            Breakfast Tuesday and Thursday 10am-12 noon. Informal advice on finding emergency accommodation,
+                                    <ol>
+                                        <li>Opening hours:</li>
+                                        <li>Monday – Friday: 10am–4pm</li>
+                                        <li>Address: 360-364 City Road, London, EC1V 2PY</li>
+                                        <li>Phone: 020 7713 7655</li>
+                                        <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.hopeworldwide.org.uk'>http://www.hopeworldwide.org.uk</a></li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div className='HelpItems'>
+                                <div className='item'>
+                                    <h5>All Saints Church, Islington</h5>
+                                    <p>Food and social contact for people who are homeless and on the margins.
+                                        Breakfast Tuesday and Thursday 10am-12 noon. Informal advice on finding emergency accommodation,
                             signposting to other organisations and other issues around homelessness.</p>
-                        <ol>
-                            <li>Opening hours: make contact first</li>
-                            <li>Address: Carnegie Street, London, N1 9QW</li>
-                            <li>Phone: 020 7837 0720</li>
-                            <li>Email: allsaints.islington@london.anglican.org</li>
-                        </ol>
-                    </div>
-                </div>
-                <div className='HelpItems'>
-                    <div className='item'>
-                        <h5>Alone in London Service</h5>
-                        <p>Support services for young people aged 16-25 who are homeless or at risk of becoming homeless.
-                            Services include: information and signposting; housing advice;
-                            advice on welfare benefits; general advocacy; help with getting into education,
-                            training & employment; counselling service;
+                                    <ol>
+                                        <li>Opening hours: make contact first</li>
+                                        <li>Address: Carnegie Street, London, N1 9QW</li>
+                                        <li>Phone: 020 7837 0720</li>
+                                        <li>Email: allsaints.islington@london.anglican.org</li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div className='HelpItems'>
+                                <div className='item'>
+                                    <h5>Alone in London Service</h5>
+                                    <p>Support services for young people aged 16-25 who are homeless or at risk of becoming homeless.
+                                        Services include: information and signposting; housing advice;
+                                        advice on welfare benefits; general advocacy; help with getting into education,
+                                        training & employment; counselling service;
                             family mediation and mediation for young ex-offenders and their families.</p>
-                        <ol>
-                            <li>Opening hours:</li>
-                            <li>Monday - Friday: 9am - 1pm, 2pm - 4pm</li>
-                            <li>Address: Endeavour Centre, Sherborne House,
+                                    <ol>
+                                        <li>Opening hours:</li>
+                                        <li>Monday - Friday: 9am - 1pm, 2pm - 4pm</li>
+                                        <li>Address: Endeavour Centre, Sherborne House,
                                 34 Decima Street, London, SE1 4QQ</li>
-                            <li>Phone: 020 7278 4224</li>
-                            <li>Email: enquiries@als.org.uk</li>
-                            <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.aloneinlondon.org'>http://www.aloneinlondon.org</a></li>
-                        </ol>
-                    </div>
-                </div>
-                <div className='HelpItems'>
-                    <div className='item'>
-                        <h5>Focus Homeless Outreach Team</h5>
-                        <p>Community mental health team offering support to homeless people with mental health problems.
+                                        <li>Phone: 020 7278 4224</li>
+                                        <li>Email: enquiries@als.org.uk</li>
+                                        <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.aloneinlondon.org'>http://www.aloneinlondon.org</a></li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div className='HelpItems'>
+                                <div className='item'>
+                                    <h5>Focus Homeless Outreach Team</h5>
+                                    <p>Community mental health team offering support to homeless people with mental health problems.
                             Street outreach work. Sessions in hostels and day centres.</p>
-                        <ol>
-                            <li>Opening hours:</li>
-                            <li>Monday - Friday: 9am - 5pm</li>
-                            <li>Address: 4 Greenland Road, London, NW1 0AS</li>
-                            <li>Phone: 020 3317 6590</li>
-                            <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.candi.nhs.uk'>http://www.candi.nhs.uk</a></li>
-                        </ol>
-                    </div>
-                </div>
-                <div className='HelpItems'>
-                    <div className='item'>
-                        <h5>Barka UK - Reconnection Project</h5>
-                        <p>Street outreach work with Eastern European rough sleepers, who are destitute. 
-                            Help people connect to homelessness day centres and work with them to prepare for 
-                            reconnection with their families and communities. 
-                            People can be referred to the Barka Network of Inclusion Program in Poland, 
-                            where they can access detox, 
+                                    <ol>
+                                        <li>Opening hours:</li>
+                                        <li>Monday - Friday: 9am - 5pm</li>
+                                        <li>Address: 4 Greenland Road, London, NW1 0AS</li>
+                                        <li>Phone: 020 3317 6590</li>
+                                        <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.candi.nhs.uk'>http://www.candi.nhs.uk</a></li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div className='HelpItems'>
+                                <div className='item'>
+                                    <h5>Barka UK - Reconnection Project</h5>
+                                    <p>Street outreach work with Eastern European rough sleepers, who are destitute.
+                                        Help people connect to homelessness day centres and work with them to prepare for
+                                        reconnection with their families and communities.
+                                        People can be referred to the Barka Network of Inclusion Program in Poland,
+                                        where they can access detox,
                             learn new work skills and build friendships with others who share their journey.</p>
-                        <ol>
-                            <li>Opening hours:</li>
-                            <li>Monday - Friday: 9am - 5pm</li>
-                            <li>Address: Lancaster House, 70 Newington Causeway, London, SE1 6DF</li>
-                            <li>Phone: 020 7275 7768</li>
-                            <li>Email: barka.uk@googlemail.com</li>
-                            <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.barkauk.org'>http://www.barkauk.org</a></li>
-                        </ol>
+                                    <ol>
+                                        <li>Opening hours:</li>
+                                        <li>Monday - Friday: 9am - 5pm</li>
+                                        <li>Address: Lancaster House, 70 Newington Causeway, London, SE1 6DF</li>
+                                        <li>Phone: 020 7275 7768</li>
+                                        <li>Email: barka.uk@googlemail.com</li>
+                                        <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.barkauk.org'>http://www.barkauk.org</a></li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <break></break>
+                            <Link to='/'>
+                                <div className="Btn"><label>Main Menu</label></div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Washing.js
+++ src/Washing.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react';
 import './Washing.css';
 import WashingIcon from './Icons/WashingIcon.svg';
+import { Link } from 'react-router-dom';
 
 class Washing extends Component {
     render() {
         return (
             <div className="body">
                 <img src={WashingIcon} className="Icon" alt="Washing Icon" />
-                <div className='WashingItems'>
-                    <div className='item'>
-                        <h4>Barons Court Project</h4>
-                        <p>Day centre for people vulnerable due to mental ill-health and/or homelessness. 
-                            Tenancy sustainment support. Cost-price cafe, laundry and shower facilities available on drop-in days. 
-                            Life skills training Thursday 2pm-5pm. Women's Group Fri 12 noon-2pm. Green Palm Club (BME social group) 
-                            Fri 2.30pm-5pm. 
+                <div><h1 className="MainHeading">Washing</h1>
+                    <div><h1 className="IntroText">Here are your search results for Washing.</h1>
+                        <div><h1 className="IntroText">To return to the main menu click on the button at the bottom of the page.</h1>
+                            <div className='WashingItems'>
+                                <div className='item'>
+                                    <h4>Barons Court Project</h4>
+                                    <p>Day centre for people vulnerable due to mental ill-health and/or homelessness.
+                                        Tenancy sustainment support. Cost-price cafe, laundry and shower facilities available on drop-in days.
+                                        Life skills training Thursday 2pm-5pm. Women's Group Fri 12 noon-2pm. Green Palm Club (BME social group)
+                                        Fri 2.30pm-5pm.
                             Activities throughout the week such as drama, relaxation, computer sessions and quizzes.</p>
-                        <ol>
-                            <li>Address: 69 Talgarth Road, London, W14 9DD</li>
-                            <li>Phone: 020 7603 5232</li>
-                            <li>Email: michael@baronscourtproject.org</li>
-                            <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.baronscourtproject.org'>http://www.baronscourtproject.org</a></li>
-                        </ol>
-                    </div>
-                    <div className='item'>
-                        <h4>Ace of Clubs</h4>
-                        <p>Day centre for the homeless, vulnerable and others in need. 
-                            Cheap meals, access to physical and mental health care, laundry, shower facilities. 
-                            Advice on benefits and housing Monday-Friday. Advice on finding accommodation. 
-                            Welfare advice. IT Suite with basic computing lessons and internet café sessions for online benefit claims and job searches. 
+                                    <ol>
+                                        <li>Address: 69 Talgarth Road, London, W14 9DD</li>
+                                        <li>Phone: 020 7603 5232</li>
+                                        <li>Email: michael@baronscourtproject.org</li>
+                                        <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.baronscourtproject.org'>http://www.baronscourtproject.org</a></li>
+                                    </ol>
+                                </div>
+                                <div className='item'>
+                                    <h4>Ace of Clubs</h4>
+                                    <p>Day centre for the homeless, vulnerable and others in need.
+                                        Cheap meals, access to physical and mental health care, laundry, shower facilities.
+                                        Advice on benefits and housing Monday-Friday. Advice on finding accommodation.
+                                        Welfare advice. IT Suite with basic computing lessons and internet café sessions for online benefit claims and job searches.
                             Food & safety classes.</p>
-                        <ol>
-                            <li>Address: St Alphonsus Road, London, SW4 7AS</li>
-                            <li>Phone: 020 7720 2811</li>
-                            <li>Email: sarahmiles@aceofclubs.org.uk</li>
-                            <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.aceofclubs.org.uk'>http://www.aceofclubs.org.uk</a></li>
-                        </ol>
+                                    <ol>
+                                        <li>Address: St Alphonsus Road, London, SW4 7AS</li>
+                                        <li>Phone: 020 7720 2811</li>
+                                        <li>Email: sarahmiles@aceofclubs.org.uk</li>
+                                        <li>Website: <a target="_blank" rel="noopener noreferrer" href='http://www.aceofclubs.org.uk'>http://www.aceofclubs.org.uk</a></li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <break></break>
+                            <Link to='/'>
+                                <div className="Btn"><label>Main Menu</label></div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
